5d0b3924d8b2b5956da10584a855ae0f00208f07
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-27 15:41:15 -0500

    commit tree 8c1cf558c982da656ccd81893b62ab288c105787

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Triangle
-  # write code here
+ attr_reader :sides
+
+  def initialize(side_a, side_b, side_c)
+    @sides = [side_a, side_b, side_c]
+    @sides.sort!
+  end
+  def kind
+      if @sides.any? {|side| side <= 0 } || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif self.sides.uniq.length == 2
+        :isosceles 
+    elsif self.sides.uniq.length == 1
+        :equilateral
+    else
+        :scalene
+    end
+  end
+
+
+
+class TriangleError < StandardError
 end
+end 
